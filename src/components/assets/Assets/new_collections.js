a920a2716c7a58c09c2d89db7162d42e25286a69
itzneeraj06 <neerajchouhan0604@gmail.com>
2023-12-23 00:35:41 +0530

    newcollection_update

--- src/components/assets/Assets/new_collections.js
+++ src/components/assets/Assets/new_collections.js
@@ -1,18 +1,18 @@
-import p1_img from "./product_12.png";
-import p2_img from "./product_35.png";
-import p3_img from "./product_14.png";
-import p4_img from "./product_8.png";
-import p5_img from "./product_15.png";
-import p6_img from "./product_20.png";
-import p7_img from "./product_17.png";
-import p8_img from "./product_28.png";
+import p2_img from "./product_2.png";
+import p8_img from "./product_8.png";
+import p12_img from "./product_12.png";
+import p14_img from "./product_14.png";
+import p15_img from "./product_15.png";
+import p17_img from "./product_17.png";
+import p28_img from "./product_28.png";
+import p35_img from "./product_35.png";
 
 let new_collections = [
   {
     id: 2,
     name: "Nike Joyride Run Flyknit",
     category: "women",
-    image: p1_img,
+    image: p2_img,
     new_price: 85.0,
     old_price: 120.5,
   },
@@ -20,7 +20,7 @@ let new_collections = [
     id: 8,
     name: "Nike Phantom Vision Elite Dynamic Fit FG",
     category: "women",
-    image: p2_img,
+    image: p8_img,
     new_price: 95.0,
     old_price: 130.5,
   },
@@ -28,7 +28,7 @@ let new_collections = [
     id: 12,
     name: "Nike Superstar Limited Edition",
     category: "women",
-    image: p3_img,
+    image: p12_img,
     new_price: 125.0,
     old_price: 150.5,
   },
@@ -37,7 +37,7 @@ let new_collections = [
     id: 14,
     name: "Nike Bridgport Advice",
     category: "men",
-    image: p4_img,
+    image: p14_img,
     new_price: 85.0,
     old_price: 120.5,
   },
@@ -45,7 +45,7 @@ let new_collections = [
     id: 15,
     name: "Nike React Miler",
     category: "men",
-    image: p5_img,
+    image: p15_img,
     new_price: 110.0,
     old_price: 125.5,
   },
@@ -54,7 +54,7 @@ let new_collections = [
     id: 17,
     name: "Club C Revenge Mens",
     category: "men",
-    image: p6_img,
+    image: p17_img,
     new_price: 75.0,
     old_price: 80.5,
   },
@@ -62,7 +62,7 @@ let new_collections = [
     id: 28,
     name: "Nike Air Max 90",
     category: "kid",
-    image: p8_img,
+    image: p28_img,
     new_price: 150.0,
     old_price: 180.5,
   },
@@ -71,7 +71,7 @@ let new_collections = [
     id: 35,
     name: "Boys Orange Colourblocked Hooded Sweatshirt",
     category: "kid",
-    image: p7_img,
+    image: p35_img,
     new_price: 85.0,
     old_price: 120.5,
   },
